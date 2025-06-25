6940346366d857169de0aeb079d71d6b84297e15
Lorenzo Montero <54688379+LoreMontero@users.noreply.github.com>
2025-06-24 20:16:40 -0400

    Revert "Fixed some issues and added fund script and uniswap protocol to frontend"

--- packages/nextjs/utils/customChains.ts
+++ packages/nextjs/utils/customChains.ts
@@ -1,21 +1,22 @@
 import { defineChain } from "viem";
 
-export const base = defineChain({
-  id: 8453,
-  name: "Base",
-  nativeCurrency: { name: "Base", symbol: "ETH", decimals: 18 },
-  rpcUrls: {
-    default: {
-      http: ["https://mainnet.base.org"],
-    },
-  },
-  blockExplorers: {
-    default: {
-      name: "Basescan",
-      url: "https://basescan.org",
-    },
-  },
-});
+// Base chain
+// export const base = defineChain({
+//   id: 8453,
+//   name: "Base",
+//   nativeCurrency: { name: "Base", symbol: "ETH", decimals: 18 },
+//   rpcUrls: {
+//     default: {
+//       http: ["https://mainnet.base.org"],
+//     },
+//   },
+//   blockExplorers: {
+//     default: {
+//       name: "Basescan",
+//       url: "https://basescan.org",
+//     },
+//   },
+// });
 
 export const baseSepolia = defineChain({
   id: 84532,
@@ -28,13 +29,13 @@ export const baseSepolia = defineChain({
   },
 });
 
-// export const baseFork = defineChain({
-//   id: 8453,
-//   name: "BaseFork",
-//   nativeCurrency: { name: "Forked Base", symbol: "ETH", decimals: 18 },
-//   rpcUrls: {
-//     default: {
-//       http: ["https://127.0.0.1:8545"],
-//     },
-//   },
-// });
+export const baseFork = defineChain({
+  id: 8453,
+  name: "BaseFork",
+  nativeCurrency: { name: "Forked Base", symbol: "ETH", decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ["https://base-mainnet.g.alchemy.com/v2/rT55BM4OEEI-4a6iH6N8U"],
+    },
+  },
+});
